5b6bbd6a278870aa953634245d09a153c8376caa
simaocunha71 <simaopscunha@outlook.pt>
2024-05-13 16:50:46 +0100

    bug fix

--- codegeex/benchmark/humaneval-x/rust/target/debug/build/libc-a5f595ef1ee1b008/build_script_build-a5f595ef1ee1b008.d
+++
@@ -1,5 +0,0 @@
-/workspace/CodeGeeX/codegeex/benchmark/rust/target/debug/build/libc-a5f595ef1ee1b008/build_script_build-a5f595ef1ee1b008: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.154/build.rs
-
-/workspace/CodeGeeX/codegeex/benchmark/rust/target/debug/build/libc-a5f595ef1ee1b008/build_script_build-a5f595ef1ee1b008.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.154/build.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.154/build.rs:
